88f5ede5bde4c9b391798392e119d214e3ae5ae1
busangangster <wlwhssla1034@naver.com>
2023-06-06 17:22:07 +0900

    Baekjoon

--- baekjoon/pra.py
+++ baekjoon/pra.py
@@ -1,7 +1,26 @@
-x = 100
-y = 80
-import math as m
-print(m.trunc((y/x)*100))
-print(m.trunc((y+6)/(x+6)*100))
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-print((y*100)//x)
+n = int(input())
+a = []
+for i in range(1,n+1):
+  a.append(i)
+
+dq = deque(a)
+k = []
+
+for _ in range(n-1):
+  cur1 = dq.popleft()
+  cur2 = dq.popleft()
+  k.append(cur1)
+  dq.append(cur2)
+
+for x in k:
+  print(x,end=' ')
+
+print(*dq)
+
+
+
+    

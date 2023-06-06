c560384b54e0562b1a71c52eb8aea41da11ca03f
busangangster <wlwhssla1034@naver.com>
2023-06-06 17:22:37 +0900

    Baekjoon

---
+++ baekjoon/구현/2161.py
@@ -0,0 +1,29 @@
+
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+a = []
+for i in range(1,n+1):
+  a.append(i)
+
+dq = deque(a)
+k = []
+
+for _ in range(n-1):
+  cur1 = dq.popleft()
+  cur2 = dq.popleft()
+  k.append(cur1)
+  dq.append(cur2)
+
+for x in k:
+  print(x,end=' ')
+
+for x in dq:
+  print(x)
+
+
+
+    

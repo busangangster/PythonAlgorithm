3522dd0bac2a2cb8317c35e141b6d2b3ca06bd44
busangangster <wlwhssla1034@naver.com>
2023-08-08 14:46:39 +0900

    Baekjoon

--- baekjoon/pra.py
+++ baekjoon/pra.py
@@ -1,34 +1,59 @@
-
-
 import sys
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-cus = []
+n = int(input())
+arr = []
+pizza1 = []
+pizza2 = []
+pizza3 = []
+cnt = 0
+count = 0
+for _ in range(n):
+    arr.append(input().strip())
 
-for _ in range(m):
-  cus.append(int(input()))
+for i in range(n):
+  if arr[i][2] == '2':
+      pizza1.append(arr[i])
+  elif arr[i][2] == '4' and arr[i][0] == '1':
+      pizza2.append(arr[i])
+  else:
+      pizza3.append(arr[i])
 
-cus.sort(reverse = True)
-max = 0 # 최대 수익
-price = 0 # 책정한 가격
+while pizza2 and pizza3:
+    pizza2.pop()
+    pizza3.pop()
+    cnt +=1 
+    
 
-for i in range(min(n,m)):
-  p = cus[i] # 최대 이익을 측정하기 위한 가격 설정 
-  num = 0 # 몇개의 달걀을 팔 수 있는지
-  for j in range(min(n,m)):
-    # 고객의 구매의사 가격이 설정가보다 높으면 달걀 팔 수 있음
-    if cus[j] >= p: 
-      num += 1
-    else:  # 내림차순 정렬했기 때문에 else문에 걸리면 그 다음 요소들도 조건문을 반족 X 
-      break  # 반복문 바로 빠져나옴
-
-  # 이중 반복문이 끝났을 때 책정 가격과 이익을 변수에 저장 
-  if p * num > max:
-    max = p*num
-    price = p
-
-print(price,max)
+while pizza1 and pizza2:
+  pizza2.pop()
+  if not pizza2:
+    pizza1.pop()
+    cnt +=1 
+    break
+  pizza2.pop()
+  pizza1.pop()
+  cnt += 1
 
 
+
+while pizza1:
+   pizza1.pop()
+   if not pizza1:
+      cnt += 1
+      break
+   pizza1.pop()
+   cnt += 1
+
+while pizza2:
+   pizza2.pop()
+   cnt += 1
+
+while pizza3:
+   pizza3.pop()
+   cnt += 1
+
+
+
+print(cnt)
 

049c9c8b0d100a8ec3c1fb54a97c0575ac672c44
busangangster <wlwhssla1034@naver.com>
2023-11-03 22:06:11 +0900

    Baekjoon

--- baekjoon/pra.py
+++ baekjoon/pra.py
@@ -1,24 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-n,x = map(int,input().split())
-a = list(map(int,input().split()))
+a = input().strip()
+b = input()
 
-if max(a) == 0:
-  print('SAD')
-
+if a <= b:
+  print('go')
 else:
-  cnt = 1
-  window = sum(a[:x])
-  max_v = window
-  for i in range(x,n):
-    window += a[i] - a[i-x]
-
-    if max_v < window:
-      max_v = window
-      cnt = 1
-    elif max_v == window:
-      cnt += 1
-
-  print(max_v)
-  print(cnt)
+  print('no')

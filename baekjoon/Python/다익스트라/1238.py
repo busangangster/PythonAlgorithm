33b86b5ad114402f2dc36ce3001dd4787aa87a81
busangangster <wlwhssla1034@naver.com>
2023-11-14 19:50:37 +0900

    Baekjoon

---
+++ baekjoon/Python/다익스트라/1238.py
@@ -0,0 +1,37 @@
+import sys
+import heapq as hq
+input = sys.stdin.readline
+
+def dijkstra(start):
+  q = []
+  min_dis = [int(1e9)] * (n+1)
+  min_dis[start] = 0
+  hq.heappush(q,[0,start])
+
+  while q:
+    cur_dis,cur_node = hq.heappop(q)
+
+    if min_dis[cur_node] < cur_dis:
+      continue
+
+    for next_node,next_dis in graph[cur_node]:
+      distance = cur_dis + next_dis
+      if min_dis[next_node] > distance:
+        min_dis[next_node] = distance
+        hq.heappush(q,[distance,next_node])
+        
+  return min_dis
+
+n,m,x = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+ans = -2147000000
+for _ in range(m):
+  a,b,c = map(int,input().split())
+  graph[a].append([b,c])
+
+for i in range(1,n+1):
+  go = dijkstra(i)
+  back = dijkstra(x)
+  ans = max(ans,go[x] + back[i])
+
+print(ans)

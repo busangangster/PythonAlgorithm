8f4167eee9cd09ac87c15ecbe16b672f5b5f7193
busangangster <wlwhssla1034@naver.com>
2023-12-09 13:40:24 +0900

    Baekjoon

--- baekjoon/Python/문자열/5582.py
+++ baekjoon/Python/문자열/5582.py
@@ -1,16 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-  number = []
-  n = int(input())
-  for _ in range(n):
-    number.append(input().strip())
-  number.sort(key = lambda x:x)
-  for i in range(n-1):
-    if number[i] == number[i+1][:len(number[i])]:
-      print('NO')
-      break
-  else:
-    print('YES')
+a = input().strip()
+b = input().strip()
+ans = 0
+dp = [[0 for _ in range(len(b)+1)]  for _ in range(len(a)+1)]
+
+for i in range(1,len(a)+1):
+  for j in range(1,len(b)+1):
+    if a[i-1] == b[j-1]:
+      dp[i][j] = dp[i-1][j-1] + 1
+      ans = max(dp[i][j],ans)
+print(ans)

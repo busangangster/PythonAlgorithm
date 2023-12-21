5c9a8def2d58f9157e32287ab8f4257dfda6cd89
busangangster <wlwhssla1034@naver.com>
2023-12-21 14:15:33 +0900

    Baekjoon

---
+++ baekjoon/Python/DP/11048.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0 for _ in range(m)] for _ in range(n)]
+r,c = 0,0
+
+for i in range(n):
+  for j in range(m):
+    dp[i][j] = max(dp[i][j-1],dp[i-1][j],dp[i][j]) + arr[i][j]
+
+print(dp[n-1][m-1])
+
+

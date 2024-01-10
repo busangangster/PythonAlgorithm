ad86456f250e268facec319e9808e30bc84be05b
busangangster <wlwhssla1034@naver.com>
2024-01-10 22:57:31 +0900

    Baekjoon

---
+++ baekjoon/Java/15873.java
@@ -0,0 +1,23 @@
+package baekjoon.Java;
+
+import java.io.*;
+import java.util.*;
+
+class P15873 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+    String s = st.nextToken();
+    int ans = 0;
+
+    if (s.charAt(1) == '0') {
+      ans = 10 + Integer.parseInt(s.substring(2)); 
+    } else {
+      ans = Integer.parseInt(s.substring(0,1)) + Integer.parseInt(s.substring(1));
+    }
+    System.out.println(ans);
+  }
+  
+}
--- baekjoon/prac.java
+++ baekjoon/prac.java
@@ -1,3 +1,9 @@
 package baekjoon;
-import java.util.Scanner;
 
+class Main {
+  public static void main(String[] args) {
+    String s = "apple";
+
+    System.out.println(s.substring(2,3));
+  }
+}

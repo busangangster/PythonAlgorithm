b40fad289f2283b69776169ee6612ac58435dd52
busangangster <wlwhssla1034@naver.com>
2024-01-10 21:03:21 +0900

    Baekjoon

---
+++ baekjoon/Java/1568.java
@@ -0,0 +1,27 @@
+package baekjoon.Java;
+import java.util.*;
+import java.io.*;
+
+class P1568 {
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int k = 1;
+    int ans = 0;
+
+    while (n != 0) {
+      if (k > n) {
+        k = 1;
+      } else {
+        n -= k;
+        k += 1;
+        ans += 1;
+      }
+    }
+
+    System.out.println(ans);
+  
+  }
+}
